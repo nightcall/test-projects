51fcf42bab594a926b4fcd4aeeacc88abd25023a
saraband <sara@band.com>
2017-12-10 20:57:30 +0100

    update

--- HttpServerCpp/main.cpp
+++ HttpServerCpp/main.cpp
@@ -4,50 +4,51 @@
 
 int main(int argc, char** argv) {
 
-	sf::TcpListener listener;
-	sf::SocketSelector selector;
-	std::list<sf::TcpSocket*> clients;
 
-	listener.listen(8080);
-	selector.add(listener);
+    sf::TcpListener listener;
+    sf::SocketSelector selector;
+    std::list<sf::TcpSocket*> clients;
 
-	while(true) {
-		if(selector.wait()) {
-			if(selector.isReady(listener)) {
-				sf::TcpSocket* client = new sf::TcpSocket;
+    listener.listen(8080);
+    selector.add(listener);
 
-				if(listener.accept(*client) == sf::TcpSocket::Done) {
-					clients.push_back(client);
-					selector.add(*client);
-					std::cout << clients.size() << std::endl;
-				} else {
-					delete client;
-				}
-			} else {
-				for(std::list<sf::TcpSocket *>::iterator it = clients.begin();
-					it != clients.end(); ++it) {
-					sf::TcpSocket& client = **it;
+    while(true) {
+        if(selector.wait()) {
+            if(selector.isReady(listener)) {
+                sf::TcpSocket* client = new sf::TcpSocket;
 
-					if(selector.isReady(client)) {
-						char buffer[1024];
-						std::size_t received = 0;
+                if(listener.accept(*client) == sf::TcpSocket::Done) {
+                    clients.push_back(client);
+                    selector.add(*client);
+                    std::cout << clients.size() << std::endl;
+                } else {
+                    delete client;
+                }
+            } else {
+                for(std::list<sf::TcpSocket *>::iterator it = clients.begin();
+                    it != clients.end(); ++it) {
+                    sf::TcpSocket& client = **it;
 
-						if(client.receive(buffer, sizeof(buffer), received) == sf::Socket::Done) {
-							
-							std::string body = "<html><head></head><body><p>Hello world !</p></body></html>";
-							std::string res = "HTTP/1.1 200 OK\n";
-							res += "Connection: Closed\n";
-							res += "Content-Length: " + std::to_string(body.size()) + "\n";
-							res += "Content-Type: text/html\n\r\n";
-							res += body;
+                    if(selector.isReady(client)) {
+                        char buffer[1024];
+                        std::size_t received = 0;
 
-							client.send(res.c_str(), res.size() + 1);
-						}
-					}
-				}
-			}
-		}
-	}
+                        if(client.receive(buffer, sizeof(buffer), received) == sf::Socket::Done) {
+                            
+                            std::string body = "<html><head></head><body><p>Hello world !</p></body></html>";
+                            std::string res = "HTTP/1.1 200 OK\n";
+                            res += "Connection: Closed\n";
+                            res += "Content-Length: " + std::to_string(body.size()) + "\n";
+                            res += "Content-Type: text/html\n\r\n";
+                            res += body;
 
-	return 0;
+                            client.send(res.c_str(), res.size() + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
 }
